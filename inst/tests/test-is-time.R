5140ae60f0957ebf9ced71d5aa204670adf7d3b2
richierocks <richierocks@gmail.com>
2013-01-05 19:43:26 +0000

    ran roxygenise; updated description, news

--- inst/tests/test-is-time.R
+++ inst/tests/test-is-time.R
@@ -1,10 +1,10 @@
 test.is_in_past.then_now_soon.returns_true_in_past <- function()
 {  
-  #Can't test _now_ (x = Sys.time()) easily because on slow 
+  #Can't test _now_ (x == Sys.time()) easily because on slow 
   #machines time has moved on, but this isn't the case for 
   #fast machines.  5secs should be plenty of leaway.
   x <- Sys.time() + c(-1, 5)
-  expected <- c(TRUE, FALSE, FALSE)
+  expected <- c(TRUE, FALSE)
   names(expected) <- x
   checkEquals(
     expected,
@@ -16,7 +16,7 @@ test.is_in_future.then_now_soon.returns_true_in_past <- function()
 {
   #See problem in test.is_in_past
   x <- Sys.time() + c(-1, 5)
-  expected <- c(FALSE, FALSE, TRUE)
+  expected <- c(FALSE, TRUE)
   names(expected) <- x
   checkEquals(
     expected,
