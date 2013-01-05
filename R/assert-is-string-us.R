f474bd0ac2e849d22b88b24eef83e508ea11a305
richierocks <richierocks@gmail.com>
2013-01-05 19:22:26 +0000

    added assert funs for is_us_telephone_number

--- R/assert-is-string-us.R
+++ R/assert-is-string-us.R
@@ -1,3 +1,20 @@
+#' @rdname is_us_telephone_number
+#' @export
+assert_all_are_us_telephone_numbers <- function(x)
+{                                                     
+  msg <- sprintf("%s are not all US telephone numbers.", get_name_in_parent(x))
+  assert_engine(x, is_us_telephone_number, msg)        
+}
+
+#' @rdname is_us_telephone_number
+#' @export
+assert_any_are_us_telephone_numbers <- function(x)
+{                                                     
+  msg <- sprintf("%s are all not US telephone numbers.", get_name_in_parent(x))
+  assert_engine(x, is_us_telephone_number, msg, what = "any")        
+}
+
+
 #' @rdname is_us_zip_code
 #' @export
 assert_all_are_us_zip_codes <- function(x)
