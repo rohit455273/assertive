9b47d317d6a71797a73d08fb1280483c38c2b21f
Richard J Cotton <rjc2003@qatar-med.cornell.edu>
2014-04-16 11:40:58 +0300

    Added tests for use_first

--- inst/tests/test-utils.R
+++ inst/tests/test-utils.R
@@ -5,4 +5,55 @@ test.coerce_to.numeric_vector_to_data_frame.returns_data_frame <- function()
   checkEquals(expected, coerce_to(x, "data.frame"))
 }
 
+test.use_first.a_vector_double_indexing.returns_first_element <- function()
+{
+  x <- letters
+  expected <- "a"
+  checkIdentical(suppressWarnings(use_first(x)), expected)
+  old_ops <- options(warn = 2)
+  on.exit(options(old_ops))
+  checkException(use_first(x))
+}
 
+test.use_first.a_vector_single_indexing.returns_first_element <- function()
+{
+  x <- letters
+  expected <- "a"
+  checkIdentical(suppressWarnings(use_first(x, "[")), expected)
+  old_ops <- options(warn = 2)
+  on.exit(options(old_ops))
+  checkException(use_first(x, "["), silent = TRUE)  
+}
+
+test.use_first.a_list_double_indexing.returns_contents_of_first_element <- function()
+{
+  x <- as.list(letters)
+  expected <- "a"
+  checkIdentical(suppressWarnings(use_first(x)), expected)
+  old_ops <- options(warn = 2)
+  on.exit(options(old_ops))
+  checkException(use_first(x), silent = TRUE)  
+}
+
+test.use_first.a_list_single_indexing.returns_first_element <- function()
+{
+  x <- as.list(letters)
+  expected <- list("a")
+  checkIdentical(suppressWarnings(use_first(x, "[")), expected)
+  old_ops <- options(warn = 2)
+  on.exit(options(old_ops))
+  checkException(use_first(x, "["), silent = TRUE)  
+}
+
+test.use_first.a_scalar.returns_x <- function()
+{
+  x <- "a"
+  expected <- x
+  checkIdentical(use_first(x), expected)
+}
+
+test.use_first.empty.throws_error <- function()
+{
+  x <- NULL
+  checkException(use_first(x), silent = TRUE)
+}
