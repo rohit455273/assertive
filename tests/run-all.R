f17a27a8cb48591bf2e5688809bdf5aee2cf0c07
Richard J Cotton <rjc2003@qatar-med.cornell.edu>
2014-04-17 08:03:33 +0300

    now 3 options: build/devel/installed

--- tests/run-all.R
+++ tests/run-all.R
@@ -1,19 +1,38 @@
 library(RUnit)
 
-is_devel <- TRUE
-if(is_devel)
-{
-  source("assertive/tests/problems.R")
-  r_files <- dir("assertive/R", pattern = "\\.R$", full.names = TRUE)
-  invisible(lapply(r_files, sys.source, envir = globalenv()))
-#   load_code("assertive")
-  test_dir <- "assertive/inst/tests"
-} else
-{
-  library(assertive)
-  source("assertive/tests/problems.R")
-  test_dir <- system.file("tests", package = "assertive") 
-}
+# This script runs all the RUnit unit tests for the assertive package.
+# To run tests from an installed version, change the mode variable to 
+# 'installed_package'. If you have checked out a development version,
+# change mode to 'devel' and set your working directory to the parent
+# of the assertive package root.  To make the package build correctly
+# change mode to 'build'.
+mode <- "build"  #build or devel or installed_package
+switch(
+  mode,
+  build = {
+    # when building the package, the current working directory is 
+    # assertive.Rcheck/tests
+    r_files <- dir("../../assertive/R", pattern = "\\.R$", full.names = TRUE)
+    invisible(lapply(r_files, sys.source, envir = globalenv()))
+    test_dir <- "../../assertive/inst/tests"       
+  },
+  devel = 
+  {
+    # current working directory assumed to be parent of assertive package root
+    # need to source contents of R dir
+    source("assertive/tests/problems.R")
+    r_files <- dir("assertive/R", pattern = "\\.R$", full.names = TRUE)
+    invisible(lapply(r_files, sys.source, envir = globalenv()))
+    test_dir <- "assertive/inst/tests"
+  },
+  installed_package = {
+    # take files from existing package installation
+    library(assertive)
+    source("assertive/tests/problems.R")
+    test_dir <- system.file("tests", package = "assertive") 
+  },
+  stop("Unknown development mode.")
+)
 
 ts <- defineTestSuite(
   "Test Assertive", 
@@ -22,8 +41,15 @@ ts <- defineTestSuite(
 )
 
 (results <- runTestSuite(ts, verbose = FALSE))
-bad_tests <- problems(results)
-if(nrow(bad_tests) > 0) View(bad_tests)
+if(with(results[["Test Assertive"]], nErr + nFail) > 0) 
+{
+  if(mode == 'build')
+  {
+    stop("Units tests have thrown errors.")
+  }
+  bad_tests <- problems(results)
+  View(bad_tests)
+}
 
 
 
