37700b1a44aa97812c1d365e338d2318095cc5a2
Richard J Cotton <rjc2003@qatar-med.cornell.edu>
2014-04-24 13:24:01 +0300

    Added functions for checking R's capabilities.

---
+++ R/r-has-capability.R
@@ -0,0 +1,186 @@
+#' Does R have a capability?
+#' 
+#' Check to see if R has a specific capability.
+#' @return The \code{is_*} functions return \code{TRUE} if R has the capability 
+#' and \code{FALSE} (with a cause) otherwise.
+#' The \code{assert_*} functions return nothing but throw an error if the 
+#' corresponding \code{is_*} function returns \code{FALSE}.
+#' @seealso \code{\link[base]{capabilities}}
+#' @alias r_has_capability
+#' @examples
+#' \dontrun{
+#' if(r_has_png_capability())
+#' {
+#'   png("test.png")
+#'   with(cars, plot(speed, dist))
+#'   dev.off()
+#' } else 
+#' {
+#'   pdf("test.pdf")
+#'   with(cars, plot(speed, dist))
+#'   dev.off()
+#' }
+#' }
+#' @export
+r_has_jpeg_capability <- function()
+{
+  if(!capabilities("jpeg"))
+  {
+    return(false("R does not have jpeg capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_png_capability <- function()
+{
+  if(!capabilities("png"))
+  {
+    return(false("R does not have png capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_tiff_capability <- function()
+{
+  if(!capabilities("tiff"))
+  {
+    return(false("R does not have tiff capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_tcltk_capability <- function()
+{
+  if(!capabilities("tcltk"))
+  {
+    return(false("R does not have tcltk capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_x11_capability <- function()
+{
+  if(!capabilities("X11"))
+  {
+    return(false("R does not have X11 capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_aqua_capability <- function()
+{
+  if(!capabilities("aqua"))
+  {
+    return(false("R does not have aqua capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_http_ftp_capability <- function()
+{
+  if(!capabilities("http/ftp"))
+  {
+    return(false("R does not have http/ftp capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_sockets_capability <- function()
+{
+  if(!capabilities("sockets"))
+  {
+    return(false("R does not have sockets capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_libxml_capability <- function()
+{
+  if(!capabilities("libxml"))
+  {
+    return(false("R does not have libxml capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_fifo_capability <- function()
+{
+  if(!capabilities("fifo"))
+  {
+    return(false("R does not have fifo capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_cledit_capability <- function()
+{
+  if(!capabilities("cledit"))
+  {
+    return(false("R does not have cledit capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_iconv_capability <- function()
+{
+  if(!capabilities("iconv"))
+  {
+    return(false("R does not have iconv capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_nls_capability <- function()
+{
+  if(!capabilities("NLS"))
+  {
+    return(false("R does not have NLS capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_profmem_capability <- function()
+{
+  if(!capabilities("profmem"))
+  {
+    return(false("R does not have profmem capability."))
+  }
+  TRUE
+}
+
+#' @rdname r_has_jpeg_capability
+#' @export
+r_has_cairo_capability <- function()
+{
+  if(!capabilities("cairo"))
+  {
+    return(false("R does not have cairo capability."))
+  }
+  TRUE
+}
