c6a612a9fc2eaf5abb0032c198428f41b12292d6
Richard J Cotton <rjc2003@qatar-med.cornell.edu>
2014-04-28 15:42:18 +0300

    manual test updates

--- inst/tests/test-is-reflection.R
+++ inst/tests/test-is-reflection.R
@@ -5,12 +5,22 @@ test_that("test.is_64_bit_os.any_os.returns_pointer_size_equals_8", {
 
 test_that("test.is_batch_mode.any_mode.returns_true_if_called_from_batch_mode", {
   expected <- !is.na(Sys.getenv("R_BATCH", NA))
-  expect_equal(is_batch_mode(), expected)
+  actual <- is_batch_mode()
+  expect_equal(strip_attributes(actual), expected)
+  if(!actual)
+  {
+    expect_equal(cause(actual), noquote("R is not running in batch mode."))
+  }
 })
 
 test_that("test.is_comma_for_decimal_point.any_locale.returns_true_if_locale_uses_comma", {
-  expected <- Sys.localeconv()["mon_decimal_point"] == ","
-  expect_equal(is_comma_for_decimal_point(), expected)
+  expected <- unname(Sys.localeconv()["mon_decimal_point"] == ",")
+  actual <- is_comma_for_decimal_point()
+  expect_equal(strip_attributes(actual), expected)
+  if(!actual)
+  {
+    expect_equal(cause(actual), noquote("The locale convention is to use a '.' for a decimal point."))
+  }
 })
 
 test_that("test.is_interactive.any_mode.returns_true_if_r_runs_interactively", {
@@ -25,7 +35,12 @@ test_that("test.is_linux.any_mode.returns_true_if_os_is_linux", {
 
 test_that("test.is_mac.any_mode.returns_true_if_os_is_osx", {
   expected <- unname(Sys.info()["sysname"] == "Darwin")
-  expect_equal(strip_attributes(is_mac()), expected)
+  actual <- is_mac()
+  expect_equal(strip_attributes(actual), expected)
+  if(!actual)
+  {
+    expect_equal(cause(actual), noquote("The operating system is not OS X (or another Darwin-based OS)."))
+  }
 })
 
 test_that("test.is_on_os_path.made_up_paths.returns_false_for_all", {
@@ -40,7 +55,12 @@ test_that("test.is_on_os_path.os_paths.returns_true_for_all", {
 
 test_that("test.is_period_for_decimal_point.any_locale.returns_true_if_locale_uses_period", {
   expected <- unname(Sys.localeconv()["mon_decimal_point"] == ".")
-  expect_equal(is_period_for_decimal_point(), expected)
+  actual <- is_period_for_decimal_point()
+  expect_equal(strip_attributes(actual), expected)
+  if(!actual)
+  {
+    expect_equal(cause(actual), noquote("The locale convention is to use a ',' for a decimal point."))
+  }
 })
 
 test_that("test.is_r.r_or_s.returns_true_if_is_r", {
@@ -50,11 +70,20 @@ test_that("test.is_r.r_or_s.returns_true_if_is_r", {
 
 test_that("test.is_unix.any_os.returns_true_if_os_is_unix_based", {
   expected <- .Platform$OS.type == "unix"
-  expect_equal(strip_attributes(is_unix()), expected)
+  actual <- is_unix()
+  expect_equal(strip_attributes(actual), expected)
+  if(!actual)
+  {
+    expect_equal(cause(actual), noquote("The operating system is not Unix-based."))
+  }
 })
 
 test_that("test.is_windows.any_os.returns_true_if_os_is_windows", {
   expected <- .Platform$OS.type == "windows"
-  expect_equal(strip_attributes(is_windows()), expected)
+  actual <- is_windows()
+  expect_equal(strip_attributes(actual), expected)
+  if(!actual)
+  {
+    expect_equal(cause(actual), noquote("The operating system is not Windows."))
+  }
 })
- 
