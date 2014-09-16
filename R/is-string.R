80d31f77b0ae7b255de61dd1bba3efaa3749f735
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-09-16 17:12:55 +0300

    is_email_address now returns cause attr.

--- R/is-string.R
+++ R/is-string.R
@@ -206,7 +206,12 @@ is_date_string <- function(x, format = "%F %T", .xname = get_name_in_parent(x))
 #' email addresses.
 #' @examples
 #' addresses <- c(
-#'   "a@@b.com", "a_at_b.com", "a@@bcom", "a@@b.comma", "a!$&@@b.com"
+#'   ok       = "a@@b.com", 
+#'   no_at    = "a_at_b.com", 
+#'   no_dot   = "a@@bcom", 
+#'   long_tld = "a@@b.comma", 
+#'   punct    = "a!$&@@b.com", 
+#'   missing  = NA
 #' )
 #' is_email_address(addresses)
 #' is_email_address(addresses, method = "rfc2822")
@@ -224,7 +229,8 @@ is_email_address <- function(x, method = c("simple", "rfc2822"),
     simple = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$",
     rfc2822 = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
   )
-  matches_regex(x, rx, perl = TRUE)
+  ok <- matches_regex(x, rx, perl = TRUE)
+  set_cause(ok, "bad format")
 }
 
 #' @rdname is_character
