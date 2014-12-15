ac08992e7027aad17f86f30fd6e1d93cc20b3a6e
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-12-15 14:59:57 +0300

    Added n_elements fn.

--- R/utils.R
+++ R/utils.R
@@ -233,6 +233,34 @@ merge_dots_with_list <- function(..., l = list())
   merge(dots, l)
 }
 
+#' Get the number of elements
+#' 
+#' Gets the number of elements in an object.
+#' @param x Any object.
+#' @return A non-negative integer of the number of elements.
+#' @note For atomic objects, the number of elements is the product of the
+#' dimensions, as calculated by \code{\link{DIM}}.  For recursive objects,
+#' the number of elements is the sum of the number of elements of each of
+#' their atomic components.
+#' @seealso \code{\link{DIM}}
+#' @examples
+#' n_elements(1:10)
+#' n_elements(NULL)
+#' n_elements(data.frame(x = 1:5, y = rnorm(5)))
+#' n_elements(list(1:5, list(1:3, list(1:7))))
+#' n_elements(var) # depends upon the length of the body
+#' @export
+n_elements <- function(x)
+{
+  if(is.recursive(x))
+  {
+    sum(vapply(x, n_elements, integer(1)))
+  } else
+  {
+    as.integer(prod(DIM(x)))
+  }  
+}
+
 #' Wrap a string in brackets
 #'
 #' Parenthesise a character vector by wrapping elements in brackets, 
