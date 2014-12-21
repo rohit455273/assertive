7fabcaa41f0b49e1070ddec9e59a41e933375859
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2014-12-21 10:36:14 +0300

    Added are_same_length + assert* fns.

--- R/assert-are-same.R
+++ R/assert-are-same.R
@@ -39,9 +39,55 @@ assert_any_are_identical <- function(...)
     handler(
       "The expressions ", 
       toString(as.list(match.call())[-1]), 
-      " are not all identical.",
+      " are all not identical.",
       call. = FALSE
     )
   }
 }
 
+#' @rdname are_same_length
+#' @export
+assert_all_are_same_length <- function(...)
+{
+  # Nasty reimplementation of functionality since assert_engine doesn't work
+  # ... inputs right now.
+  ok <- are_same_length(...)
+  if(!all(ok))
+  {
+    handler <- match.fun(
+      match.arg(
+        getOption("assertive.severity"), 
+        c("stop", "warning", "message")
+      )
+    )
+    handler(
+      "The expressions ", 
+      toString(as.list(match.call())[-1]), 
+      " are not all the same length.",
+      call. = FALSE
+    )
+  }
+}
+
+#' @rdname are_same_length
+#' @export
+assert_any_are_same_length <- function(...)
+{
+  # Also nasty.
+  ok <- are_same_length(...)
+  if(!any(ok))
+  {
+    handler <- match.fun(
+      match.arg(
+        getOption("assertive.severity"), 
+        c("stop", "warning", "message")
+      )
+    )
+    handler(
+      "The expressions ", 
+      toString(as.list(match.call())[-1]), 
+      " are all not the same length.",
+      call. = FALSE
+    )
+  }
+}
