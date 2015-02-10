2fe1b2741817af649cd0bf2e72155ba602b54f67
richierocks <richierocks@gmail.com>
2015-02-10 23:22:47 +0300

    is_real, is_imaginary gain a tol argument

--- R/assert-is-real-imaginary.R
+++ R/assert-is-real-imaginary.R
@@ -1,31 +1,31 @@
 #' @rdname is_real
 #' @export
-assert_all_are_imaginary <- function(x)
+assert_all_are_imaginary <- function(x, tol = 100 * .Machine$double.eps)
 {                                                     
   msg <- sprintf("%s are not all imaginary.", get_name_in_parent(x))
-  assert_engine(x, is_imaginary, msg)        
+  assert_engine(x, is_imaginary, msg, tol = tol)        
 }
 
 #' @rdname is_real
 #' @export
-assert_any_are_imaginary <- function(x)
+assert_any_are_imaginary <- function(x, tol = 100 * .Machine$double.eps)
 {                                                     
   msg <- sprintf("%s are all not imaginary.", get_name_in_parent(x))
-  assert_engine(x, is_imaginary, msg, what = "any")        
+  assert_engine(x, is_imaginary, msg, what = "any", tol = tol)        
 }
 
 #' @rdname is_real
 #' @export
-assert_all_are_real <- function(x)
+assert_all_are_real <- function(x, tol = 100 * .Machine$double.eps)
 {                                                     
   msg <- sprintf("%s are not all real.", get_name_in_parent(x))
-  assert_engine(x, is_real, msg)        
+  assert_engine(x, is_real, msg, tol = tol)        
 }
 
 #' @rdname is_real
 #' @export
-assert_any_are_real <- function(x)
+assert_any_are_real <- function(x, tol = 100 * .Machine$double.eps)
 {                                                     
   msg <- sprintf("%s are all not real.", get_name_in_parent(x))
-  assert_engine(x, is_real, msg, what = "any")        
+  assert_engine(x, is_real, msg, what = "any", tol = tol)        
 }
