b1af3024804810c3612da4feea4d937b58cdab75
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-03-16 08:35:00 +0300

    Intro vignette now works, is built.

---
+++ inst/doc/introduction.R
@@ -0,0 +1,11 @@
+## ----, Setup, echo = FALSE, results = "hide"-----------------------------
+library(assertive)
+knitr::opts_chunk$set(error = FALSE)
+
+## ----, IsNumeric---------------------------------------------------------
+is_numeric(1:6)
+is_numeric(letters)
+
+## ----, IsNonNegative-----------------------------------------------------
+is_non_negative(rnorm(6))
+
