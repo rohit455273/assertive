c34db360e4f58479a4df8debcf1cf5c1dcdeec23
richierocks <richierocks@gmail.com>
2015-07-08 04:57:21 +0200

    completed set of is-true/false/na fns; some code reorg

--- R/is-identical-to-true-false-na.R
+++ R/is-identical-to-true-false-na.R
@@ -23,7 +23,11 @@ is_identical_to_na <- function(x, allow_attributes = FALSE,
   {
     x <- strip_attributes(x)
   }
-  if(!identical(NA, as.logical(x)))
+  if(!identical(NA, x) && 
+     !identical(NA_real_, x) && 
+     !identical(NA_character_, x) && 
+     !identical(NA_integer_, x) && 
+     !identical(NA_complex_, x))
   {
     return(false("%s is not identical to NA.", .xname))
   }
