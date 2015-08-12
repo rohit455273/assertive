bcfcb974304693b6c14701d3f1e66370c860e989
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-12 10:41:02 +0300

    ripped out assertions for numbers

--- R/is-other.R
+++ R/is-other.R
@@ -23,39 +23,6 @@ is_debugged <- function(x, .xname = get_name_in_parent(x))
   TRUE
 }
 
-#' Is the input divisible by a number?
-#' 
-#' Checks to see if the input is divisible by some number.
-#' @param x A numeric vector to divide.
-#' @param n A numeric vector to divide by.
-#' @param tol Differences from zero smaller than \code{tol} are not considered.
-#' @param na_ignore A logical value.  If \code{FALSE}, \code{NA} values
-#' cause an error; otherwise they do not.  Like \code{na.rm} in many
-#' stats package functions, except that the position of the failing
-#' values does not change.
-#' @return \code{TRUE} if the input \code{x} is divisible by \code{n}, within 
-#' the specified tolerance.
-#' @note \code{is_even} and \code{is_odd} are shortcuts for divisibility by two.
-#' @seealso \code{is_whole_number}
-#' @examples
-#' is_divisible_by(1:10, 3)
-#' is_divisible_by(-5:5, -2)
-#' is_divisible_by(1.5:10.5, c(1.5, 3.5))
-#' assert_any_are_even(1:10)
-#' dont_stop(assert_all_are_even(1:10))
-#' @export
-is_divisible_by <- function(x, n, tol = 100 * .Machine$double.eps)
-{
-  call_and_name(
-    function(x) 
-    {
-      ok <- abs(x %% n) <= tol
-      set_cause(ok, "indivisible")
-    }, 
-    x
-  ) 
-}
-
 #' Does the code run without throwing an error?
 #' 
 #' Call the code inside a try block and report if an error was thrown.
@@ -75,13 +42,6 @@ is_error_free <- function(x)
   ok <- TRUE
   attr(ok, "result") <- res
   ok
-}
-
-#' @rdname is_divisible_by
-#' @export
-is_even <- function(x, tol = 100 * .Machine$double.eps)
-{
-  is_divisible_by(x, 2L, tol = tol)  
 }
 
 #' Does the variable exist?
@@ -192,14 +152,6 @@ is_loaded <- function(x, PACKAGE = "", type = "",
   }
 }
 
-#' @rdname is_divisible_by
-#' @importFrom stats setNames
-#' @export
-is_odd <- function(x, tol = 100 * .Machine$double.eps)
-{
-  setNames(is_divisible_by(x - 1, 2L, tol = tol), x)
-}
-
 #' Is the input a symmetric matrix?
 #'
 #' Checks that the input is a symmetric matrix.
@@ -291,38 +243,3 @@ is_unsorted <- function(x, na.rm = FALSE, strictly = FALSE,
   TRUE
 }
 
-#' Is the input a whole number?
-#'
-#' Checks that the (probably floating point) input is a whole number.
-#' 
-#' @param x Input to check.
-#' @param tol Differences smaller than \code{tol} are not considered.
-#' @param na_ignore A logical value.  If \code{FALSE}, \code{NA} values
-#' cause an error; otherwise they do not.  Like \code{na.rm} in many
-#' stats package functions, except that the position of the failing
-#' values does not change.
-#' @note The term whole number is used to distinguish from integer in
-#' that the input \code{x} need not have type \code{integer}.  In fact
-#' it is expected that \code{x} will be \code{numeric}.
-#' @return \code{TRUE} if the input is a whole number.
-#' @seealso \code{is_divisible_by}
-#' @examples
-#' # 1, plus or minus a very small number
-#' x <- 1 + c(0, .Machine$double.eps, -.Machine$double.neg.eps)
-#' # By default, you get a bit of tolerance for rounding errors
-#' is_whole_number(x)
-#' # Set the tolerance to zero for exact matching.
-#' is_whole_number(x, tol = 0)
-#' @export
-is_whole_number <- function(x, tol = 100 * .Machine$double.eps)
-{
-  x <- coerce_to(x, "numeric")
-  call_and_name(
-    function(x) 
-    {
-      ok <- abs(x - round(x)) <= tol & !is.infinite(x)
-      set_cause(ok, ifelse(is.infinite(x), "infinite", "fractional"))
-    }, 
-    x
-  )
-}
