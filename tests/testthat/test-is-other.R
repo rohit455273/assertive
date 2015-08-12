6d39dfe07f7c9a25ddee87b20e6cf5bb39850816
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-12 13:52:28 +0300

    ripped out assertive.code files

--- tests/testthat/test-is-other.R
+++ tests/testthat/test-is-other.R
@@ -1,25 +1,3 @@
-test_that("test.is_debugged.a_function.returns_true_when_debugged", {
-  x <- function() {
-  }
-  expect_false(is_debugged(x))
-  debug(x)
-  expect_true(is_debugged(x))
-  undebug(x)
-  expect_false(is_debugged(x))
-})
-
-test_that("test.is_existing.some_variables.returns_true_when_they_exist", {
-  e <- new.env()
-  e$a_variable <- 1
-  x <- c("a_variable", "not_a_variable")
-  expected <- c(TRUE, FALSE)
-  names(expected) <- x
-  # actual needs to be calculated in its own line, not inside
-  # expect_equal, or the parent frame is wrong
-  actual <- is_existing(x, envir = e, inherits = FALSE)
-  expect_equal(actual, expected, label = paste("actual = ", toString(deparse(actual))))
-})
-
 test_that("test.is_symmetric_matrix.a_symmetric_matrix.returns_logical", {
   x <- diag(3)
   x[3, 1] <- 1e-100
