42f5aed8d4d7400248032983e9c906a6abbb302b
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-12 16:56:48 +0300

    ripped out matrix, set fns

--- R/assert-is-other.R
+++ R/assert-is-other.R
@@ -1,16 +1,3 @@
-#' @rdname is_symmetric_matrix
-#' @export
-assert_is_symmetric_matrix <- function(x, tol = 100 * .Machine$double.eps, ...)
-{                                                         
-  assert_engine(
-    is_symmetric_matrix, 
-    x, 
-    tol = tol, 
-    .xname = get_name_in_parent(x),
-    ...
-  )       
-}
-
 #' @rdname is_unsorted
 #' @export
 assert_is_unsorted <- function(x, na.rm = FALSE, strictly = FALSE)
