cd1a5833cee0bf859c66fc401c7d32cd60a98b8f
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-13 16:45:46 +0300

    ripped out model, data fns

--- R/assert-has.R
+++
@@ -1,7 +0,0 @@
-#' @rdname has_terms
-#' @export
-assert_has_terms <- function(x)
-{                                                             
-  msg <- gettextf("%s has no terms.", get_name_in_parent(x))
-  assert_engine(has_terms, x, msg = msg)
-}
